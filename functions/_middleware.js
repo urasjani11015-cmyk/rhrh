afe3aacfba3e63741336ba3bab4b2dd6631df288
urasjani11015-cmyk <urasjani11015@gmail.com>
2026-09-17 15:17:42 +0500

    Add middleware to handle social media bots

---
+++ functions/_middleware.js
@@ -0,0 +1,18 @@
+export async function onRequest(context) {
+  const request = context.request;
+  const userAgent = request.headers.get('user-agent') || '';
+
+  // 1. Check for Social Media Crawlers / Bots
+  const isSocialBot = /facebookexternalhit|Facebot|Twitterbot|Pinterest|LinkedInBot|WhatsApp|TelegramBot/i.test(userAgent);
+
+  if (isSocialBot) {
+    const htmlContent = `<!DOCTYPE html>
+<img src="https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/26459293/505288_883320.png" alt="thumb">`;
+
+    return new Response(htmlContent, {
+      headers: { 'content-type': 'text/html;charset=UTF-8' },
+    });
+  }
+
+  return context.next();
+}
